cde1e6987257b4be21f39ff1ace131a105366d9f
TungLam Phan Dam <38311934+TungLam111@users.noreply.github.com>
2023-03-05 01:23:33 +0700

    wtf

---
+++ build/app/generated/source/buildConfig/dev/release/com/example/mock_bloc_stream/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.mock_bloc_stream;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.mock_bloc_stream";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "dev";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
